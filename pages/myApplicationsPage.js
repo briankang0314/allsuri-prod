ee244777a96efeef227d20700c52cf20f3d474da
Byungsoo Kang <briankang0314@gmail.com>
2024-10-02 04:58:18 +0900

    declaring currentPage in myApplications

--- pages/myApplicationsPage.js
+++ pages/myApplicationsPage.js
@@ -11,7 +11,7 @@ let myApplicationsCurrentFilters = {
 
 let myApplicationsCurrentSort = 'created_at';
 
-
+let currentPage = 1;
 
 export async function SetupMyApplicationsPage() {
     console.log('Setting up my applications page');
